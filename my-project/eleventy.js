d18fa88e16ca83ef821b1d5b6d083c1ce4196bca
crystallchenn <crystalchen1368@gmail.com>
2022-03-21 13:10:41 -0400

    aknfa

--- my-project/eleventy.js
+++ my-project/eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
      eleventyConfig.addPassthroughCopy("src/styles");
-     eleventyConfig.addPassthroughCopy("src/js");
+     eleventyConfig.addPassthroughCopy("src/");
     return{
         dir:{
             input:"src",
