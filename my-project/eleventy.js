4b4ac4f8d7e5f0f451076c421e8b3d2f704b5ee3
julianne444 <90789875+julianne444@users.noreply.github.com>
2022-04-21 15:33:38 -0400

    fdsa

--- my-project/eleventy.js
+++ my-project/eleventy.js
@@ -1,10 +1,11 @@
-module.exports = function(eleventyConfig) {
-     eleventyConfig.addPassthroughCopy("src/styles");
-     eleventyConfig.addPassthroughCopy("src/js");
-    return{
-        dir:{
-            input:"src",
-            output:"dist",
-        },
-    };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/styles");
+  eleventyConfig.addPassthroughCopy("src/md");
+  eleventyConfig.addPassthroughCopy("src/js");
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
 };
