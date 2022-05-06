58bd1e402b84284b3ea22f7e7c2968e47794338a
julianne444 <90789875+julianne444@users.noreply.github.com>
2022-05-06 13:08:36 -0400

    OGM

--- design-scaffold/.eleventy.js
+++ design-scaffold/.eleventy.js
@@ -3,7 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style");
   eleventyConfig.addPassthroughCopy("src/md");
   eleventyConfig.addPassthroughCopy("src/media");
-
+  require("nunjucks");
   return {
     dir: {
       input: "src",
