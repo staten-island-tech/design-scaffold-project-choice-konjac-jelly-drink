dad9d01a6eead39282df40880a1d2a54c20dcece
crystallchenn <crystalchen1368@gmail.com>
2022-05-09 13:10:34 -0400

    qihfiawf

--- design-scaffold/.eleventy.js
+++ design-scaffold/.eleventy.js
@@ -3,9 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style");
   eleventyConfig.addPassthroughCopy("src/md");
   eleventyConfig.addPassthroughCopy("src/media");
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-  });
+  
   return {
     dir: {
       input: "src",
